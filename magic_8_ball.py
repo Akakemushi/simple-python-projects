6185392f46c69ba64500f0bcb1886692f726444f
Jason Sturgeon <zergyjanus@gmail.com>
2024-09-27 10:55:49 +0900

    made magic 8 ball, and added notes

---
+++ magic_8_ball.py
@@ -0,0 +1,24 @@
+import random
+
+messages = ['Ya got me, bud.',
+    'Why would you even ask that??',
+    'I dunno, maybe?',
+    'Hell yeah!',
+    "It doesn't look good...",
+    "I don't think so.",
+    "Well, it depends...",
+    "That's a big affirmative, soldier.",
+    "Negatory.",
+    "Hell nah.",
+    "Nope.",
+    "Nu-uh.",
+    "Uh-huh!",
+    "Does a bear shit in the woods?",
+    "When pigs fly.",
+    "What a stupid question.",
+    "You're smart, you figure it out.",
+    "What do I look like? A magic 8 ball??",
+    "Insert $20 for the answer.",
+    "Yup yup yup!"]
+
+print(random.choice(messages))
---
+++ test_code.py
@@ -0,0 +1,10 @@
+array = ["dog",
+         "diddy",
+                             "pigs",
+"wtf"]
+print(array)
+
+number =  3 + 3 + 3 + 3 + \
+    3 + 3 \
+      +                   3
+print(number)
