759dba0db9bb98b5dd1cbf4b58522a707b150b26
Jason Sturgeon <zergyjanus@gmail.com>
2024-09-29 16:08:45 +0900

    notes and a simple test program

--- test_code.py
+++ test_code.py
@@ -1,10 +1,13 @@
-array = ["dog",
-         "diddy",
-                             "pigs",
-"wtf"]
-print(array)
-
-number =  3 + 3 + 3 + 3 + \
-    3 + 3 \
-      +                   3
-print(number)
+spam = ["apples", "bananas", "tofu", "cats"]
+length = len(spam)
+final_index = length - 1
+if length > 0:
+    for index, item in enumerate(spam):
+        if index == final_index:
+            print(item)
+        elif index == final_index - 1:
+            print(item + " and ", end="")
+        else:
+            print(item + ", ", end="")
+else:
+    print("This list is empty.")
