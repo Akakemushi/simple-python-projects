af93b7c67381157364cccbf3470b6c23e74cdaa8
Jason Sturgeon <zergyjanus@gmail.com>
2024-10-01 06:45:01 +0900

    working on an image matrix rotator

---
+++ rotate_image.py
@@ -0,0 +1,20 @@
+import copy
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def rotate_image(matrix):
+    # copy = copy.deepcopy(matrix)
+    for col, char in enumerate(matrix[0]):
+        for row in matrix:
+            print(row[col], end="")
+        print("")
+
+rotate_image(grid)
