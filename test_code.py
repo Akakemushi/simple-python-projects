753ab3df5ef81add63589d92dc37705baa84ac10
Jason Sturgeon <zergyjanus@gmail.com>
2024-11-15 15:12:00 +0900

    did a looooot of scary installing of stuff... fingers crossed

--- test_code.py
+++ test_code.py
@@ -1,1 +1,6 @@
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+browser = webdriver.Chrome()
+type(browser)
+browser.get('https://inventwithpython.com')
+# "/mnt/c/Program Files/Google/Chrome/Application"
