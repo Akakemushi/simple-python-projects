7eb1245527978a6a72bcd74aac43a24496fb786e
Jason Sturgeon <zergyjanus@gmail.com>
2024-11-17 00:24:42 +0900

    managed to finally get ezsheets and the api working, (it was definitely NOT ez...)

--- test_code.py
+++ test_code.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-browser = webdriver.Chrome()
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=r'/snap/bin/chromium')
+options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+print("one")
+browser = webdriver.Chrome(service=service, options=options)
+print("two")
 type(browser)
+print("three")
 browser.get('https://inventwithpython.com')
+print("four")
 # "/mnt/c/Program Files/Google/Chrome/Application"
---
+++ test_code2.py
@@ -0,0 +1,3 @@
+import ezsheets
+ss = ezsheets.Spreadsheet('1ckUwF5Co1I0N5gxNwrDM94KbIvlUZy600NUoy9fdYYg')
+print(ss.title)
