e5b3b09e0c7b59b9fab757f54a34df29df71ae70
Jason Sturgeon <zergyjanus@gmail.com>
2024-11-29 19:16:04 +0900

    stopwatch program and some notes about time and datetime modules

--- test_code2.py
+++ test_code2.py
@@ -1,28 +1,8 @@
-pythonInt = 50
-pythonStr = 'A big dog'
-pythonFloat = 2.31
-pythonBool = True
-pythonNone = None
-pythonHash = {'Daddy': 'Jason', 'Mommy': 'Ayami', 'Kid': 'Sean'}
-pythonArray = ['Sturge', 27, 5.55, 'Rain']
-
-import json
-jsonInt = json.dumps(pythonInt)
-jsonStr = json.dumps(pythonStr)
-jsonFloat = json.dumps(pythonFloat)
-jsonBool = json.dumps(pythonBool)
-jsonNone = json.dumps(pythonNone)
-jsonHash = json.dumps(pythonHash)
-jsonArray = json.dumps(pythonArray)
-
-print(jsonInt)
-print(jsonStr)
-print(jsonFloat)
-print(jsonBool)
-print(jsonNone)
-print(jsonHash)
-print(jsonArray)
-
-print(type(jsonInt))
-print(type(jsonFloat))
-print(type(jsonHash))
+import time
+print("Go!")
+time.sleep(1)
+print("Stop!")
+time.sleep(1)
+print("Go!")
+time.sleep(3.452)
+print("Stop!")
