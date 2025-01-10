8fdd93a642c8acc10af91c9b7784aeb4727162aa
Jason Sturgeon <zergyjanus@gmail.com>
2025-01-10 22:56:53 +0900

    adding more notes and work with threads

--- test_code2.py
+++ test_code2.py
@@ -1,8 +1,4 @@
-import threading, time
-print('Start of program.')
-def takeANap():
-    time.sleep(10)
-    print('Wake up!')
-threadObj = threading.Thread(target=takeANap)
-threadObj.start()
-print('End of program.')
+x = ("a", "b")
+y = ("c", "d")
+z = x + y
+print(z)
